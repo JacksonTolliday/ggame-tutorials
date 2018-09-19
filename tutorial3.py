b5d23e70634bdd02715b52106889db4fca1d376e
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-09-19 12:06:14 -0400

    Updated from Brython Server: 9/19/2018 12:06:14 PM

--- tutorial3.py
+++ tutorial3.py
@@ -14,4 +14,12 @@ noline = LineStyle(0, black)
 # a rectangle asset and sprite to use as background
 bg_asset = RectangleAsset(myapp.width, myapp.height, noline, green)
 bg = Sprite(bg_asset, (0,0))
+# A ball! This is already in the ggame-tutorials repository
+ball_asset = ImageAsset("images/orb-150545_640.png")
+ball = Sprite(ball_asset, (0, 0))
+# Original image is too big. Scale it to 1/10 its original size
+ball.scale = 0.1
+# custom attributes
+ball.direction = 1
+ball.go = True
 myapp.run()
