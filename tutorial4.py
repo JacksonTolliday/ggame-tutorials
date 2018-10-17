3a8a74573b19634319a1bcca2c1c67f9394e71b4
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-17 11:26:13 -0400

    Updated from Brython Server: 10/17/2018 11:26:13 AM

--- tutorial4.py
+++ tutorial4.py
@@ -21,9 +21,9 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        SpaceShip((100,100))            #]aaaaaa
-        SpaceShip((150,150))            #]aaaaaa   what's happening? y u no ship?
-        SpaceShip((200,50))             #]aaaaaa
+        SpaceShip((100,100))            # ]aaaaaa                                    ]aaaaaa
+        SpaceShip((150,150))            # ]aaaaaa   what's happening? y u no ship?   ]aaaaaa
+        SpaceShip((200,50))             # ]aaaaaa                                    ]aaaaaa
 
 
 myapp = SpaceGame()
