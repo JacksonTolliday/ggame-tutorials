f3155eee0cbfae1a11e486ece8c576431f1f6626
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-19 11:41:17 -0400

    Updated from Brython Server: 10/19/2018 11:41:17 AM

--- tutorial4.py
+++ tutorial4.py
@@ -5,14 +5,14 @@ class SpaceShip(Sprite):
     Animated space ship
     """
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
-        Frame(227,0,65,125), 4, 'vertical')                                     # PROBLEMA CON ESA LINEA?
+        Frame(227,0,65,125), 4, 'vertical')
 
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
         self.vx = 1
         self.vy = 1
         self.vr = 0.01
-        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("ke ydown", "space", self.thrustOn)
         SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
         self.fxcenter = self.fycenter = 0.5
 
@@ -40,16 +40,16 @@ class SpaceGame(App):
     """
     Tutorial4 space game example.
     """
-    def __init__(self):                                                 #like what even is __init__???
-        super().__init__()                                                      #so yeah i don't get what I'm doing enough to know how to fix the problem
+    def __init__(self):
+        super().__init__()
         # Background
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(self.width, self.height, noline, black)           #ok then        I DID NOTHING WHY DOES IT WORK NOW?!
+        bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        SpaceShip((100,100))            #] aaaaaa?!                                    ] aaaaaa?!
-        SpaceShip((150,150))            #] aaaaaa?!   what's happening? y u no ship?   ] aaaaaa?!
-        SpaceShip((200,50))             #] aaaaaa?!                                    ] aaaaaa?!
+        SpaceShip((100,100))
+        SpaceShip((150,150))
+        SpaceShip((200,50))
         self.thrust = 0
         self.thrustframe = 1
 
